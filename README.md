0934a7345c4a604a100e73ba79e24f1717659573
erolj <eroljoudy@erto.co>
2020-06-24 22:11:36 +0800

    Add images w/o alt + buttons w/o aria-label

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# template
-Git template
+# cssaudit.css
+
+## How to use
+```html
+<link rel="stylesheet" href="https://raw.githubusercontent.com/erolj/cssaudit/master/cssaudit.css" crossorigin="anonymous">
+```
--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "» Push updates to GIT.."
+echo "» Update cssaudit.css..."
+cp -f src/cssaudit.css cssaudit.css && sleep 1
+echo "» Push updates to GIT..."
 git add . && sleep 1
 echo "Apa yang diubah kali ini?"
 read -r GIT_COMMENT && sleep 1
